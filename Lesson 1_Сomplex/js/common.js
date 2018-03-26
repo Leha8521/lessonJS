be3802460178aef27e717c71427a3ec963d7dcfa
Alexey <leha8521@mail.ru>
2018-03-26 23:58:17 +0500

    Четвертый коммит

--- Lesson 1_Сomplex/js/common.js
+++ Lesson 1_Сomplex/js/common.js
@@ -2,7 +2,7 @@ var num    = 33721,
     output = [],
     sNum   = num.toString(),
     summa;
-
+    
 
     output.push(+sNum.charAt(0));
     output.push(+sNum.charAt(1));
@@ -12,6 +12,7 @@ var num    = 33721,
 
    
    summa = output[0] + output[1] + output[2] + output[3] + output[4];
+   
 
-    console.log(summa);
+    console.log('Сумма числа 33721 = ' + summa, 'Число ' + summa + ' в третий степени = ' + summa**3);
 
