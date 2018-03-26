18b22535eb7c5fd74fe4e76c6a79150adbc2b960
Alexey <leha8521@mail.ru>
2018-03-26 23:48:54 +0500

    Третий коммит

---
+++ Lesson 1_Сomplex/js/common.js
@@ -0,0 +1,17 @@
+var num    = 33721,
+    output = [],
+    sNum   = num.toString(),
+    summa;
+
+
+    output.push(+sNum.charAt(0));
+    output.push(+sNum.charAt(1));
+    output.push(+sNum.charAt(2));
+    output.push(+sNum.charAt(3));
+    output.push(+sNum.charAt(4));
+
+   
+   summa = output[0] + output[1] + output[2] + output[3] + output[4];
+
+    console.log(summa);
+
