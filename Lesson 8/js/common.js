b1b8b247e7642892dbcb00701de309b318bab0f5
Alexey <leha8521@mail.ru>
2018-04-05 00:11:25 +0500

    Мой десятый комминт

---
+++ Lesson 8/js/common.js
@@ -0,0 +1,7 @@
+let box = document.querySelector('.box'),
+    width = box.clientWidth,
+    height = box.clientHeight,
+    btn = document.getElementsByTagName('button')[0];
+
+
+console.log(document.documentElement.clientHeight);
